a77823f8631c8990e529008e6041d559f121d423
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2023-09-18 20:33:42 +0530

    Add files via upload

---
+++ Sum_of_two_digit.cpp
@@ -0,0 +1,15 @@
+// In this we will not take input from the user we will just assign and add both the number
+
+#include<iostream>
+
+using namespace std;
+
+int  main(){
+int a=10;
+int b=12;
+int c;
+c=a+b;
+cout<< " The Sum of "<<a<<" + "<<b<<" = "<<c<<endl;
+
+return 0;
+}
