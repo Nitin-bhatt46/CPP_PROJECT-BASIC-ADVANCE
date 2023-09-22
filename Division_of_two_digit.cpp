dec09a446a1bf9e25210abec5c4a2711b88acbc4
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2023-09-22 13:41:07 +0530

    Add files via upload

---
+++ Division_of_two_digit.cpp
@@ -0,0 +1,8 @@
+// without user input division
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=2;
+    cout<<"The product is "<< a<<" / "<<b<<" = "<<a/b<<endl;
+}
---
+++ Multiplication_of_two_digit.cpp
@@ -0,0 +1,8 @@
+// without user input multiplication
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=2;
+    cout<<"The product is "<< a<<" X "<<b<<" = "<<a*b<<endl;
+}
